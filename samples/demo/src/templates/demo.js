73f5f3f0791ad837c0374e00a57dc09ade9d2802
Eugene Cheltsov <e.cheltsov@msk-wire-macbook-pro-2-e.cheltsov.mail.msk>
2016-11-02 20:33:33 +0300

    Some tags

--- samples/demo/src/templates/demo.js
+++ samples/demo/src/templates/demo.js
@@ -1,39 +1,46 @@
-module.exports = function(ast$6be7f4de) {
+module.exports = function(f$77813ff9) {
+  var v$77813ff9_repo = {
+    "test-template": function(params) {
+      return [f$77813ff9('div', {
+        "data-id": ("" + (params.id))
+      }, [f$77813ff9('h3', {}, [("" + (params.text))])])];
+    }
+  };
   return function(json) {
-    return [ast$6be7f4de('div', {}, [ast$6be7f4de('style', {}, ['.preview {\nbackground: #EEE;\npadding: 20px;\nbox-sizing: border-box;\nborder: 1px solid #CCC;\n}']), ast$6be7f4de('h1', {}, ['Demo']), ast$6be7f4de('h2', {}, ['fest:value']), ast$6be7f4de('div', {
+    return [f$77813ff9('div', {}, [v$77813ff9_repo['test-template']({}), f$77813ff9('style', {}, ['.preview {\nbackground: #EEE;\npadding: 20px;\nbox-sizing: border-box;\nborder: 1px solid #CCC;\n}']), f$77813ff9('h1', {}, ['Demo']), f$77813ff9('h2', {}, ['fest:value']), f$77813ff9('div', {
       "class": "preview"
-    }, [("" + (json.hello))]), ast$6be7f4de('h2', {}, ['fest:each']), ast$6be7f4de('div', {
+    }, [("" + (json.hello))]), f$77813ff9('h2', {}, ['fest:each']), f$77813ff9('div', {
       "class": "preview"
-    }, [ast$6be7f4de('ul', {}, [(function() {
-      var ast$6be7f4de$__1 = [],
+    }, [f$77813ff9('ul', {}, [(function() {
+      var v$77813ff9_1 = [],
         key, value;
       for (key in json.previewEach) {
         if (json.previewEach.hasOwnProperty(key)) {
           value = json.previewEach[key];
-          ast$6be7f4de$__1.push([ast$6be7f4de('li', {}, [("" + (key)), '=', ("" + (value))])]);
+          v$77813ff9_1.push([f$77813ff9('li', {}, [("" + (key)), '=', ("" + (value))])]);
         }
       }
-      return ast$6be7f4de$__1;
-    }())])]), ast$6be7f4de('h2', {}, ['fest:for']), ast$6be7f4de('div', {
+      return v$77813ff9_1;
+    }())])]), f$77813ff9('h2', {}, ['fest:for']), f$77813ff9('div', {
       "class": "preview"
-    }, [ast$6be7f4de('ul', {}, [(function() {
-      var ast$6be7f4de$__2 = [],
+    }, [f$77813ff9('ul', {}, [(function() {
+      var v$77813ff9_2 = [],
         i, value;
       for (i in json.previewFor) {
         if (json.previewFor.hasOwnProperty(i)) {
           value = json.previewFor[i];
-          ast$6be7f4de$__2.push([ast$6be7f4de('li', {}, [("" + (value))])]);
+          v$77813ff9_2.push([f$77813ff9('li', {}, [("" + (value))])]);
         }
       }
-      return ast$6be7f4de$__2;
-    }())])]), ast$6be7f4de('h2', {}, ['fest:if']), ast$6be7f4de('div', {
+      return v$77813ff9_2;
+    }())])]), f$77813ff9('h2', {}, ['fest:if']), f$77813ff9('div', {
       "class": "preview"
     }, [(function() {
-      var ast$6be7f4de$__3 = [];
+      var v$77813ff9_3 = [];
       if (json.previewIf) {
-        ast$6be7f4de$__3.push('true');
+        v$77813ff9_3.push('true');
       };
-      return ast$6be7f4de$__3;
+      return v$77813ff9_3;
     }())])])];
   };
 };
--- samples/todo/src/ts/app.ts
+++ samples/todo/src/ts/app.ts
@@ -2,13 +2,23 @@ import tplTodos = require('../templates/todos');
 import { createVDOM } from '../../../../dist/vdom/element';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const state = {
+  const $root = document.getElementById('root');
+  const vdom = createVDOM($root, tplTodos);
+
+  let state = {
     label: 'Initial state',
     newTodo: '',
     todos: []
   };
-  
-  const $root = document.getElementById('root');
-  const vdom = createVDOM($root, tplTodos);
+
   vdom.update(state);
+  let lastId = 0;
+
+  setInterval(() => {
+    state.todos = [{
+      id: ++lastId,
+      text: Math.random()
+    }].concat(state.todos)
+    vdom.update(state);
+  }, 5000);
 });
