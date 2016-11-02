d5b510a301b866f42a268a9643abcb79b861f92b
Eugene Cheltsov <e.cheltsov@echeltsov.local>
2016-11-03 02:45:38 +0300

    sample

--- samples/demo/src/templates/demo.js
+++ samples/demo/src/templates/demo.js
@@ -1,46 +1,48 @@
-module.exports = function(f$77813ff9) {
-  var v$77813ff9_repo = {
+module.exports = function(f$ca0fb338) {
+  var v$ca0fb338_templates = {
     "test-template": function(params) {
-      return [f$77813ff9('div', {
+      return [f$ca0fb338('div', {
         "data-id": ("" + (params.id))
-      }, [f$77813ff9('h3', {}, [("" + (params.text))])])];
+      }, [f$ca0fb338('h3', {}, [("" + (params.text))])])];
     }
   };
   return function(json) {
-    return [f$77813ff9('div', {}, [v$77813ff9_repo['test-template']({}), f$77813ff9('style', {}, ['.preview {\nbackground: #EEE;\npadding: 20px;\nbox-sizing: border-box;\nborder: 1px solid #CCC;\n}']), f$77813ff9('h1', {}, ['Demo']), f$77813ff9('h2', {}, ['fest:value']), f$77813ff9('div', {
+    return [f$ca0fb338('div', {}, [f$ca0fb338('style', {}, ['.preview {\nbackground: #EEE;\npadding: 10px;\nbox-sizing: border-box;\nborder: 1px solid #CCC;\n}']), f$ca0fb338('h1', {}, ['Demo']), f$ca0fb338('h2', {}, ['fest:value']), f$ca0fb338('div', {
       "class": "preview"
-    }, [("" + (json.hello))]), f$77813ff9('h2', {}, ['fest:each']), f$77813ff9('div', {
+    }, [("" + (json.hello))]), f$ca0fb338('h2', {}, ['fest:each']), f$ca0fb338('div', {
       "class": "preview"
-    }, [f$77813ff9('ul', {}, [(function() {
-      var v$77813ff9_1 = [],
+    }, [f$ca0fb338('ul', {}, [(function() {
+      var v$ca0fb338_1 = [],
         key, value;
       for (key in json.previewEach) {
         if (json.previewEach.hasOwnProperty(key)) {
           value = json.previewEach[key];
-          v$77813ff9_1.push([f$77813ff9('li', {}, [("" + (key)), '=', ("" + (value))])]);
+          v$ca0fb338_1.push([f$ca0fb338('li', {}, [("" + (key)), '=', ("" + (value))])]);
         }
       }
-      return v$77813ff9_1;
-    }())])]), f$77813ff9('h2', {}, ['fest:for']), f$77813ff9('div', {
+      return v$ca0fb338_1;
+    }())])]), f$ca0fb338('h2', {}, ['fest:for']), f$ca0fb338('div', {
       "class": "preview"
-    }, [f$77813ff9('ul', {}, [(function() {
-      var v$77813ff9_2 = [],
+    }, [f$ca0fb338('ul', {}, [(function() {
+      var v$ca0fb338_2 = [],
         i, value;
       for (i in json.previewFor) {
         if (json.previewFor.hasOwnProperty(i)) {
           value = json.previewFor[i];
-          v$77813ff9_2.push([f$77813ff9('li', {}, [("" + (value))])]);
+          v$ca0fb338_2.push([f$ca0fb338('li', {}, [("" + (value))])]);
         }
       }
-      return v$77813ff9_2;
-    }())])]), f$77813ff9('h2', {}, ['fest:if']), f$77813ff9('div', {
+      return v$ca0fb338_2;
+    }())])]), f$ca0fb338('h2', {}, ['fest:if']), f$ca0fb338('div', {
       "class": "preview"
     }, [(function() {
-      var v$77813ff9_3 = [];
+      var v$ca0fb338_3 = [];
       if (json.previewIf) {
-        v$77813ff9_3.push('true');
+        v$ca0fb338_3.push('true');
       };
-      return v$77813ff9_3;
-    }())])])];
+      return v$ca0fb338_3;
+    }())]), f$ca0fb338('h2', {}, ['fest:set / fest:get']), f$ca0fb338('div', {
+      "class": "preview"
+    }, [v$ca0fb338_templates['test-template']({}), v$ca0fb338_templates['test-template']({})])])];
   };
 };
