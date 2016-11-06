33ba9801cfcce75842eb46adcbdafc73e4e0da48
Eugene Cheltsov <e.cheltsov@echeltsov.local>
2016-11-07 02:42:23 +0300

    wip

--- samples/demo/src/templates/demo.js
+++ samples/demo/src/templates/demo.js
@@ -1,49 +1,53 @@
-module.exports = function(f$64effc6c) {
-  var v$64effc6c_templates = {
+module.exports = function(f$bc23d178) {
+
+  // --- fest prolog ---
+  var v$bc23d178$templates = {
     "test-template": function(params) {
-      return [f$64effc6c('div', {
+      return [f$bc23d178('#fragment', {}, [f$bc23d178('div', {
         "data-id": ("" + (params.id))
-      }, [f$64effc6c('h3', {}, [("" + (params.text))])])];
+      }, [f$bc23d178('h3', {}, [('' + params.text)])])])];
     }
   };
-  var v$64effc6c_globals = {};
+  // --- end of fest prolog ---
+
+
   return function(json) {
-    return [f$64effc6c('div', {}, [f$64effc6c('style', {}, [f$64effc6c('#text', {}, ['.preview {        \nbackground: #EEE;        \npadding: 10px;        \nbox-sizing: border-box;        \nborder: 1px solid #CCC;      \n}    '])]), f$64effc6c('h1', {}, [f$64effc6c('#text', {}, ['Demo'])]), f$64effc6c('h2', {}, [f$64effc6c('#text', {}, ['fest:value'])]), f$64effc6c('div', {
+    return f$bc23d178('#fragment', {}, [f$bc23d178('div', {}, [f$bc23d178('style', {}, [f$bc23d178('#text', {}, ['.preview {background: #EEE;padding: 10px;box-sizing: border-box;border: 1px solid #CCC;}'])]), f$bc23d178('h1', {}, [f$bc23d178('#text', {}, ['Demo'])]), f$bc23d178('h2', {}, [f$bc23d178('#text', {}, ['fest:value'])]), f$bc23d178('div', {
       "class": "preview"
-    }, [("" + (json.hello))]), f$64effc6c('h2', {}, [f$64effc6c('#text', {}, ['fest:each'])]), f$64effc6c('div', {
+    }, [('' + json.hello)]), f$bc23d178('h2', {}, [f$bc23d178('#text', {}, ['fest:each'])]), f$bc23d178('div', {
       "class": "preview"
-    }, [f$64effc6c('ul', {}, [(function() {
-      var v$64effc6c_1 = [],
+    }, [f$bc23d178('ul', {}, [(function() {
+      var v$bc23d178$1 = [],
         key, value;
       for (key in json.previewEach) {
         if (json.previewEach.hasOwnProperty(key)) {
           value = json.previewEach[key];
-          v$64effc6c_1.push([f$64effc6c('li', {}, [("" + (key)), f$64effc6c('#text', {}, ['= ']), ("" + (value))])]);
+          v$bc23d178$1.push([f$bc23d178('li', {}, [('' + key), f$bc23d178('#text', {}, ['=']), ('' + value)])]);
         }
       }
-      return v$64effc6c_1;
-    }())])]), f$64effc6c('h2', {}, [f$64effc6c('#text', {}, ['fest:for'])]), f$64effc6c('div', {
+      return v$bc23d178$1;
+    }())])]), f$bc23d178('h2', {}, [f$bc23d178('#text', {}, ['fest:for'])]), f$bc23d178('div', {
       "class": "preview"
-    }, [f$64effc6c('ul', {}, [(function() {
-      var v$64effc6c_2 = [],
+    }, [f$bc23d178('ul', {}, [(function() {
+      var v$bc23d178$2 = [],
         i, value;
       for (i in json.previewFor) {
         if (json.previewFor.hasOwnProperty(i)) {
           value = json.previewFor[i];
-          v$64effc6c_2.push([f$64effc6c('li', {}, [("" + (value))])]);
+          v$bc23d178$2.push([f$bc23d178('li', {}, [('' + value)])]);
         }
       }
-      return v$64effc6c_2;
-    }())])]), f$64effc6c('h2', {}, [f$64effc6c('#text', {}, ['fest:if'])]), f$64effc6c('div', {
+      return v$bc23d178$2;
+    }())])]), f$bc23d178('h2', {}, [f$bc23d178('#text', {}, ['fest:if'])]), f$bc23d178('div', {
       "class": "preview"
     }, [(function() {
-      var v$64effc6c_3 = [];
+      var v$bc23d178$3 = [];
       if (json.previewIf) {
-        v$64effc6c_3.push(f$64effc6c('#text', {}, ['true      ']));
+        v$bc23d178$3.push(f$bc23d178('#text', {}, ['true']));
       };
-      return v$64effc6c_3;
-    }())]), f$64effc6c('h2', {}, [f$64effc6c('#text', {}, ['fest:set / fest:get'])]), f$64effc6c('div', {
+      return v$bc23d178$3;
+    }())]), f$bc23d178('h2', {}, [f$bc23d178('#text', {}, ['fest:set / fest:get'])]), f$bc23d178('div', {
       "class": "preview"
-    }, [v$64effc6c_templates['test-template']({}), v$64effc6c_templates['test-template']({})])])];
-  };
+    }, [v$bc23d178$templates['test-template']({}), v$bc23d178$templates['test-template']({})])])]);
+  };;
 };
